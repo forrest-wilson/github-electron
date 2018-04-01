2284507db292403e67999c09068cda58f99f9019
Forrest Wilson <forrest.wilson@me.com>
2018-04-01 13:45:42 +1200

    Persisting window position

--- loginWindow.js
+++ loginWindow.js
@@ -1,14 +1,25 @@
 const {BrowserWindow} = require("electron");
+const config = require("./config");
+const loginWinPosition = "loginWinPosition";
 
 exports.loginWin = null;
 
 exports.createWindow = () => {
-    this.loginWin = new BrowserWindow({
+    let windowSettings = {
         width: 400,
         height: 300,
         resizable: false,
         frame: false
-    });
+    };
+
+    let pos = config.get(loginWinPosition);
+
+    if (pos) {
+        windowSettings.x = pos.x;
+        windowSettings.y = pos.y;
+    }
+
+    this.loginWin = new BrowserWindow(windowSettings);
 
     this.loginWin.webContents.openDevTools();
 
@@ -16,5 +27,11 @@ exports.createWindow = () => {
 
     this.loginWin.on("closed", () => {
         this.loginWin = null;
+    });
+
+    this.loginWin.on("close", () => {
+        console.log("Closing");
+        let position = this.loginWin.getPosition();
+        config.set(loginWinPosition, {x: position[0], y: position[1]});
     });
 };
