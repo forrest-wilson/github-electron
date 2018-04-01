24695797f42316cacfef15a4e12e6ace528e380a
Forrest Wilson <forrest.wilson@me.com>
2018-04-01 20:41:24 +1200

    Parsing basic data ant injecting into the page

--- mainRenderer/app.js
+++ mainRenderer/app.js
@@ -1,1 +1,8 @@
 const {ipcRenderer} = require("electron");
+const config = require("../config");
+let userProps = config.get("userProps");
+
+console.log(userProps);
+
+$("#navImg").attr("src", userProps.avatar_url);
+$("#navName").text(userProps.name);
