df2041f73d5fbe890c0a79fa5b5ea29b023c0602
Forrest Wilson <forrest.wilson@me.com>
2018-04-02 13:28:22 +1200

    Saving the position and size of the mainWin BrowserWindow

--- mainWindow.js
+++ mainWindow.js
@@ -1,14 +1,27 @@
 const {BrowserWindow} = require("electron");
+const config = require("./config");
+const mainWinPosition = "mainWinPosition";
 
 exports.mainWin = null;
 
 exports.createWindow = () => {
-    this.mainWin = new BrowserWindow({
+    let windowSettings = {
         minWidth: 750,
         minHeight: 500,
         width: 1000,
         height: 700
-    });
+    }
+
+    let storedSettings = config.get(mainWinPosition);
+
+    if (storedSettings) {
+        windowSettings.width = storedSettings.width;
+        windowSettings.height = storedSettings.height;
+        windowSettings.x = storedSettings.x;
+        windowSettings.y = storedSettings.y;
+    }
+
+    this.mainWin = new BrowserWindow(windowSettings);
 
     this.mainWin.webContents.openDevTools();
 
@@ -16,5 +29,11 @@ exports.createWindow = () => {
 
     this.mainWin.on("closed", () => {
         this.mainWin = null;
+    });
+
+    this.mainWin.on("close", () => {
+        let size = this.mainWin.getSize();
+        let position = this.mainWin.getPosition();
+        config.set(mainWinPosition, {x: position[0], y: position[1], width: size[0], height: size[1]});
     });
 };
