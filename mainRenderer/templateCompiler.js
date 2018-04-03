bd0c5d09f53a7480bf3703e9baae1f95e2a5674b
Forrest Wilson <forrest.wilson@me.com>
2018-04-04 11:33:52 +1200

    Added repo-uuid data attribute to each .repo div

--- mainRenderer/templateCompiler.js
+++ mainRenderer/templateCompiler.js
@@ -4,15 +4,16 @@ exports.compileRepos = (repos) => {
     if (!repos.length) repos = [repos];
 
     repos.forEach(repo => {
-        const template = `<div class="repo content">
-                            <div class="details">
-                                <h2>${repo.name}</h2>
-                                <span>${repo.language}</span>
-                            </div>
-                            <button class="button is-success clone-button" data-cloneurl="${repo.clone_url}" data-reponame="${repo.name}">Clone Repo</button>
-                        </div><hr>`;
-        
-        $("#reposWrapper").append(template);
+        if (!document.querySelector(`[data-repouuid="${repo.id}"]`)) {
+            const template = `<div class="repo content" data-repouuid="${repo.id}">
+                                <div class="details">
+                                    <h2>${repo.name}</h2>
+                                    <span>${repo.language}</span>
+                                </div>
+                                <button class="button is-success clone-button" data-cloneurl="${repo.clone_url}" data-reponame="${repo.name}">Clone Repo</button>
+                            </div><hr>`;
+            $("#reposWrapper").append(template);
+        }
     });
 };
 
